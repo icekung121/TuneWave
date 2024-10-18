73c1d482d0d434f791ff199a3bf504336fdacef7
icekung121 <56482746+icekung121@users.noreply.github.com>
2024-10-18 09:14:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     token: process.env.TOKEN || "",
     clientID: process.env.CLIENT_ID || "", 
-    prefix: process.env.PREFIX || ".", 
+    prefix: process.env.PREFIX || "/", 
     ownerID: process.env.OWNER_ID || "",
     SpotifyID: process.env.SPOTIFY_ID || "",
     SpotifySecret: process.env.SPOTIFY_SECRET || "",
